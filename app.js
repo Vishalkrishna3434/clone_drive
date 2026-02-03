4de6f068160706a6b9b39d2a1ac5a647efbe5028
Vishalkrishna3434 <vishalthonukunooru@gmail.com>
2026-02-03 23:29:59 +0530

    used express validator to check username,password

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const userRoute=require('./routes/user.routes')
 const app=express();
 
 app.set("view engine",'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
   console.log("Hello world");
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,28 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
 
 /* /user/test */
 
-router.get("/test",(req,res)=>{
+router.get("/register", (req, res) => {
   res.render('register');
 })
 
-module.exports=router;
+router.post("/register",
+  body('email').trim().isEmail(),
+  body('password').trim().isLength({ min: 5 }),
+  body('username').trim().isLength({ min: 3 }),
+  (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: "Invalid data"
+      });
+    }
+
+    res.send(errors);
+  })
+
+module.exports = router;
